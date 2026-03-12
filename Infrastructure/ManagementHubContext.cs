0351a834a96fdce805ec7f719c6de35c8dbc7f55
ManzanoYG <manzanoygonzalezmathias.pro@gmail.com>
2026-03-12 16:04:35 +0100

    Add db context and connection string (development)

---
+++ Infrastructure/ManagementHubContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public class ManagementHubContext : DbContext
+    {
+        public ManagementHubContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
--- MyManagmentHub_API/Program.cs
+++ MyManagmentHub_API/Program.cs
@@ -1,3 +1,6 @@
+using Infrastructure;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -5,6 +8,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<ManagementHubContext>(m => m.UseSqlServer(
+    builder.Configuration.GetConnectionString("db")
+));
 
 var app = builder.Build();
 
