e3de35037168c6cb4d5fcb3fdeb87562048c0c66
ManzanoYG <manzanoygonzalezmathias.pro@gmail.com>
2026-03-13 01:36:16 +0100

    User-database connections

---
+++ Domain/Entities/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public DateTime Created_at { get; set; }
+        public DateTime Updated_at { get; set; }
+        public bool IsBanned { get; set; }
+        public byte UserType { get; set; }
+    }
+}
--- Infrastructure/ManagementHubContext.cs
+++ Infrastructure/ManagementHubContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Infrastructure.Ef.DbEntities;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -12,9 +13,21 @@ namespace Infrastructure
     {
         public ManagementHubContext(DbContextOptions options) : base(options) { }
 
+        public DbSet<DbUser> Users { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-
+            modelBuilder.Entity<DbUser>(entity =>
+            {
+                entity.ToTable("users");
+                entity.HasKey(u => u.Username);
+                entity.Property(u => u.Username).HasColumnName("username");
+                entity.Property(u => u.Password).HasColumnName("password");
+                entity.Property(u => u.Created_at).HasColumnName("created_at");
+                entity.Property(u => u.Updated_at).HasColumnName("updated_at");
+                entity.Property(u => u.IsBanned).HasColumnName("isBanned");
+                entity.Property(u => u.UserType).HasColumnName("userType");
+            });
         }
     }
 }
