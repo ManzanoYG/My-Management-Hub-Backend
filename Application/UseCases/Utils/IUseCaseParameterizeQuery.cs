699ba61b6174ecab9da4d0caa8644859b976e345
ManzanoYG <manzanoygonzalezmathias.pro@gmail.com>
2026-03-13 01:37:07 +0100

    Create 3 utils class for the use case

---
+++ Application/UseCases/Utils/IUseCaseParameterizeQuery.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.UseCases.Utils
+{
+    public interface IUseCaseParameterizeQuery<out TOutput, in TParam>
+    {
+        TOutput Execute(TParam param);
+    }
+    public interface IUseCaseParameterizeQuery<out TOutput, in TParam, in TParam2>
+    {
+        TOutput Execute(TParam param, TParam2 param2);
+    }
+    public interface IUseCaseParameterizeQuery<out TOutput, in TParam, in TParam2, in TParam3>
+    {
+        TOutput Execute(TParam param, TParam2 param2, TParam3 param3);
+    }
+}
---
+++ Application/UseCases/Utils/IUseCaseWriter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.UseCases.Utils
+{
+    public interface IUseCaseWriter<out TOutput, in TInput>
+    {
+        TOutput Execute(TInput input);
+    }
+}
