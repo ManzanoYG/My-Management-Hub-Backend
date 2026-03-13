df0a27c4f10fa6e431b9d030313d2a85797edd30
ManzanoYG <manzanoygonzalezmathias.pro@gmail.com>
2026-03-13 01:39:19 +0100

    Add configuration and services

--- MyManagmentHub_API/Program.cs
+++ MyManagmentHub_API/Program.cs
@@ -1,17 +1,46 @@
+using Application;
+using Application.UseCases.User;
 using Infrastructure;
+using Infrastructure.Ef.Authentication;
+using Infrastructure.Ef.User;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Read Config Files
+var configs = new ConfigurationBuilder()
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("appsettings.Development.json")
+    .Build();
 
+// Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddAutoMapper(typeof(Mapper));
+
 builder.Services.AddDbContext<ManagementHubContext>(m => m.UseSqlServer(
     builder.Configuration.GetConnectionString("db")
 ));
+
+//Repository
+builder.Services.AddScoped<IUserRepository, UserRepository>();
+builder.Services.AddScoped<PasswordHasher>();
+
+//User
+builder.Services.AddScoped<UseCaseCreateUser>();
+builder.Services.AddScoped<UseCaseFetchUserByUsername>();
+
+// Initialize Loggers
+builder.Services.AddLogging(b =>
+{
+    b.AddConsole();
+    b.AddDebug();
+});
+
+//SignalR
+builder.Services.AddSignalR();
 
 var app = builder.Build();
 
@@ -26,6 +55,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseRouting();
 
 app.UseAuthorization();
 
