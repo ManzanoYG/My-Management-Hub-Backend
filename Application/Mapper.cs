d4008b27a1ff0ec4df49debaca4ce7bd459c6775
ManzanoYG <manzanoygonzalezmathias.pro@gmail.com>
2026-03-13 01:38:16 +0100

    Map user with dto

---
+++ Application/Mapper.cs
@@ -0,0 +1,23 @@
+﻿using Application.UseCases.User.Dto;
+using AutoMapper;
+using Domain.Entities;
+using Infrastructure.Ef.DbEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application
+{
+    public class Mapper : Profile
+    {
+        public Mapper() 
+        {
+            //User
+            CreateMap<User, DtoOutputUser>();
+            CreateMap<DbUser, DtoOutputUser>();
+            CreateMap<DbUser, User>();
+        }
+    }
+}
