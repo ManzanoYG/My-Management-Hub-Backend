ee418d946eb0191f417a883cb54ef5b58404d74f
ManzanoYG <manzanoygonzalezmathias.pro@gmail.com>
2026-03-13 01:37:27 +0100

    Dto to create and output user

---
+++ Application/UseCases/User/Dto/DtoInputCreateUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.UseCases.User.Dto
+{
+    public class DtoInputCreateUser
+    {
+        [Required] public string UserName { get; set; }
+        [Required] public string Password { get; set; }
+    }
+}
---
+++ Application/UseCases/User/Dto/DtoOutputUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.UseCases.User.Dto
+{
+    public class DtoOutputUser
+    {
+        public string Username { get; set; }
+        public DateTime Created_at { get; set; }
+        public byte UserType { get; set; }
+    }
+}
