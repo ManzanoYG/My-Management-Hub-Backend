d99f6a1ef204b2f2b195a98c0821de6d91a2f62a
ManzanoYG <manzanoygonzalezmathias.pro@gmail.com>
2026-03-13 01:37:55 +0100

    Use case to create and fetch user by username

---
+++ Application/UseCases/User/UseCaseCreateUser.cs
@@ -0,0 +1,31 @@
+﻿using Application.UseCases.User.Dto;
+using Application.UseCases.Utils;
+using AutoMapper;
+using Infrastructure.Ef.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.UseCases.User
+{
+    public class UseCaseCreateUser : IUseCaseWriter<DtoOutputUser, DtoInputCreateUser>
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+
+        public UseCaseCreateUser(IUserRepository userRepository, IMapper mapper)
+        {
+            _userRepository = userRepository;
+            _mapper = mapper;
+        }
+
+        
+        public DtoOutputUser Execute(DtoInputCreateUser input)
+        {
+            var dbUser = _userRepository.Create(input.UserName, input.Password);
+            return _mapper.Map<DtoOutputUser>(dbUser);
+        }
+    }
+}
---
+++ Application/UseCases/User/UseCaseFetchUserByUsername.cs
@@ -0,0 +1,30 @@
+﻿using Application.UseCases.User.Dto;
+using Application.UseCases.Utils;
+using AutoMapper;
+using Infrastructure.Ef.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.UseCases.User
+{
+    public class UseCaseFetchUserByUsername : IUseCaseParameterizeQuery<DtoOutputUser, string>
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+
+        public UseCaseFetchUserByUsername(IUserRepository userRepository, IMapper mapper)
+        {
+            _userRepository = userRepository;
+            _mapper = mapper;
+        }
+
+        public DtoOutputUser Execute(string username)
+        {
+            var dbUser = _userRepository.FetchByUsername(username);
+            return _mapper.Map<DtoOutputUser>(dbUser);
+        }
+    }
+}
