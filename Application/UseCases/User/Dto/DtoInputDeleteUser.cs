d94d83f8d0a5cfada2195c08b7a8393c6a9b06a5
ManzanoYG <manzanoygonzalezmathias.pro@gmail.com>
2026-04-01 16:31:56 +0200

    Dto to input and ouput delete user data

---
+++ Application/UseCases/User/Dto/DtoInputDeleteUser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.UseCases.User.Dto
+{
+    public class DtoInputDeleteUser
+    {
+        [Required] public string Username { get; set; }
+    }
+}
