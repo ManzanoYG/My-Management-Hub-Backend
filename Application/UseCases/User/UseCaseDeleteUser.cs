f44b1e524c894ff8752ad5644319d566c56769b6
ManzanoYG <manzanoygonzalezmathias.pro@gmail.com>
2026-04-01 16:32:45 +0200

    Create use case delete user

---
+++ Application/UseCases/User/UseCaseDeleteUser.cs
@@ -0,0 +1,34 @@
+﻿using Application.UseCases.User.Dto;
+using Application.UseCases.Utils;
+using AutoMapper;
+using Infrastructure.Ef.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.UseCases.User
+{
+    public class UseCaseDeleteUser : IUseCaseParameterizeQuery<DtoOutputDeleteUser, DtoInputDeleteUser>
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+
+        public UseCaseDeleteUser(IUserRepository userRepository, IMapper mapper)
+        {
+            _userRepository = userRepository;
+            _mapper = mapper;
+        }
+
+        public DtoOutputDeleteUser Execute(DtoInputDeleteUser input)
+        {
+            Console.WriteLine(input.Username);
+            var dbUser = _userRepository.Delete(input.Username);
+            return _mapper.Map<DtoOutputDeleteUser>(new DtoOutputDeleteUser
+            {
+                Deleted = dbUser
+            });
+        }
+    }
+}
