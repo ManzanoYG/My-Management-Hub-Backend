d12412151ec1ce6d2fa46294abe56fee190026f6
ManzanoYG <manzanoygonzalezmathias.pro@gmail.com>
2026-04-01 16:32:59 +0200

    Add use case delete user to user controller

--- MyManagmentHub_API/Controllers/UserController.cs
+++ MyManagmentHub_API/Controllers/UserController.cs
@@ -1,5 +1,6 @@
 ﻿using Application.UseCases.User;
 using Application.UseCases.User.Dto;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace MyManagementHub_API.Controllers
@@ -11,12 +12,14 @@ namespace MyManagementHub_API.Controllers
         private readonly UseCaseCreateUser _useCaseCreateUser;
         private readonly UseCaseFetchUserByUsername _useCaseFetchUserByUsername;
         private readonly UseCaseChangePassword _useCaseChangePassword;
+        private readonly UseCaseDeleteUser _useCaseDeleteUser;
 
-        public UserController(UseCaseCreateUser useCaseCreateUser, UseCaseFetchUserByUsername useCaseFetchUserByUsername, UseCaseChangePassword useCaseChangePassword)
+        public UserController(UseCaseCreateUser useCaseCreateUser, UseCaseFetchUserByUsername useCaseFetchUserByUsername, UseCaseChangePassword useCaseChangePassword, UseCaseDeleteUser useCaseDeleteUser)
         {
             _useCaseCreateUser = useCaseCreateUser;
             _useCaseFetchUserByUsername = useCaseFetchUserByUsername;
             _useCaseChangePassword = useCaseChangePassword;
+            _useCaseDeleteUser = useCaseDeleteUser;
         }
 
         [HttpPost]
@@ -52,16 +55,31 @@ namespace MyManagementHub_API.Controllers
 
         [HttpPut]
         [Route("changePassword")]
-        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public ActionResult ChangePassword([FromBody] DtoInputChangePassword changePassword)
+        public ActionResult<DtoOutputChangePassword> ChangePassword([FromBody] DtoInputChangePassword changePassword)
         {
             var output = _useCaseChangePassword.Execute(changePassword);
-            return CreatedAtAction(
-                nameof(FetchByUsername),
-                new {username = output},
-                output
-                );
+            if(!output.PasswordChanged)
+                return NotFound(output);
+
+            return Ok(output);
+        }
+
+        [HttpDelete("{username}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<DtoOutputDeleteUser> Delete(string username)
+        {
+            var result = _useCaseDeleteUser.Execute(new DtoInputDeleteUser
+            {
+                Username = username
+            });
+
+            if (!result.Deleted)
+                return NotFound(result);
+
+            return Ok(result);
         }
 
     }
