ec063dd00fbe18bc90ef5681ed16db813a99af82
ManzanoYG <manzanoygonzalezmathias.pro@gmail.com>
2026-04-07 16:31:56 +0200

    Dto to get all pinned and not archived note using pagination

---
+++ Application/UseCases/Note/Dto/DtoOutputCreateNote.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.UseCases.Note.Dto
+{
+    public class DtoOutputCreateNote
+    {
+        public Guid UserId { get; set; }
+        public string Title { get; set; }
+    }
+}
