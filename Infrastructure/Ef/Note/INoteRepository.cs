5c74bcc1c0d49f69bdf7c73aa9d59ca5be1d5198
ManzanoYG <manzanoygonzalezmathias.pro@gmail.com>
2026-04-07 16:32:33 +0200

    Create note repository to get and create note

---
+++ Infrastructure/Ef/Note/INoteRepository.cs
@@ -0,0 +1,11 @@
+﻿using Infrastructure.Ef.DbEntities;
+using System.Collections;
+
+namespace Infrastructure.Ef.Note
+{
+    public interface INoteRepository
+    {
+        DbNote Create(Guid UserId, string Title, string Content, string Style);
+        List<DbNote> GetAllPinned(Guid userId, int pageNumber, int pageSize);
+    }
+}
---
+++ Infrastructure/Ef/Note/NoteRepository.cs
@@ -0,0 +1,49 @@
+﻿using Infrastructure.Ef.DbEntities;
+using Infrastructure.Services.CryptoService;
+using System.Collections;
+
+namespace Infrastructure.Ef.Note
+{
+    public class NoteRepository : INoteRepository
+    {
+        private readonly ManagementHubContext _context;
+        private readonly CryptoService _cryptoService;
+
+        public NoteRepository(ManagementHubContext context, CryptoService cryptoService)
+        {
+            _context = context;
+            _cryptoService = cryptoService;
+        }
+
+        public DbNote Create(Guid UserId, string Title, string Content, string Style)
+        {
+            var note = new DbNote
+            {
+                UserId = UserId,
+                Title = Title,
+                Content = _cryptoService.Encrypt(Content),
+                Style = Style
+            };
+            _context.Notes.Add(note);
+            _context.SaveChanges();
+            return note;
+        }
+
+        public List<DbNote> GetAllPinned(Guid userId, int pageNumber, int pageSize)
+        {
+            var notes = _context.Notes
+                .Where(n => n.UserId == userId && n.IsPinned && !n.IsArchived)
+                .OrderByDescending(n => n.CreatedAt)
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .ToList();
+
+            foreach (var note in notes)
+            {
+                note.Content = _cryptoService.Decrypt(note.Content);
+            }
+
+            return notes;
+        }
+    }
+}
