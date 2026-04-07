7a95a6eb618077439011403126b15aa51f899100
ManzanoYG <manzanoygonzalezmathias.pro@gmail.com>
2026-04-07 16:36:12 +0200

    Create use case to create and get note

---
+++ Application/UseCases/Note/UseCaseCreateNote.cs
@@ -0,0 +1,30 @@
+﻿using Application.UseCases.Note.Dto;
+using Application.UseCases.Utils;
+using AutoMapper;
+using Infrastructure.Ef.Note;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.UseCases.Note
+{
+    public class UseCaseCreateNote : IUseCaseParameterizeQuery<DtoOutputCreateNote, DtoInputCreateNote, Guid>
+    {
+        private readonly INoteRepository _noteRepository;
+        private readonly IMapper _mapper;
+
+        public UseCaseCreateNote(IMapper mapper, INoteRepository noteRepository)
+        {
+            _mapper = mapper;
+            _noteRepository = noteRepository;
+        }
+
+        public DtoOutputCreateNote Execute(DtoInputCreateNote input, Guid userId)
+        {
+            var dbNote = _noteRepository.Create(userId, input.Title, input.Content, input.Style);
+            return _mapper.Map<DtoOutputCreateNote>(dbNote);
+        }
+    }
+}
---
+++ Application/UseCases/Note/UseCaseGetAllNotePinned.cs
@@ -0,0 +1,30 @@
+﻿using Application.UseCases.Note.Dto;
+using Application.UseCases.Utils;
+using AutoMapper;
+using Infrastructure.Ef.Note;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.UseCases.Note
+{
+    public class UseCaseGetAllNotePinned : IUseCaseParameterizeQuery<List<DtoOutputGetAllPinned>, DtoInputGetAllPinned>
+    {
+        private readonly INoteRepository _noteRepository;
+        private readonly IMapper _mapper;
+
+        public UseCaseGetAllNotePinned(IMapper mapper, INoteRepository noteRepository)
+        {
+            _mapper = mapper;
+            _noteRepository = noteRepository;
+        }
+
+        public List<DtoOutputGetAllPinned> Execute(DtoInputGetAllPinned input)
+        {
+            var dbNotes = _noteRepository.GetAllPinned(input.userId, input.pageNumber, input.pageSize);
+            return _mapper.Map<List<DtoOutputGetAllPinned>>(dbNotes);
+        }
+    }
+}
