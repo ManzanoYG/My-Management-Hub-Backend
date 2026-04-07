9efe46d6ecec38fa081acc8175a7209534c29310
ManzanoYG <manzanoygonzalezmathias.pro@gmail.com>
2026-04-07 16:30:48 +0200

    Dto to create a new note

---
+++ Application/UseCases/Note/Dto/DtoInputCreateNote.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.UseCases.Note.Dto
+{
+    public class DtoInputCreateNote
+    {
+        [Required] public string Title { get; set; }
+        [Required] public string Content { get; set; }
+        public string Style { get; set; }
+    }
+}
---
+++ Application/UseCases/Note/Dto/DtoInputGetAllPinned.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.UseCases.Note.Dto
+{
+    public class DtoInputGetAllPinned
+    {
+        [Required] public Guid userId { get; set; }
+        [Range(1, int.MaxValue)] public int pageNumber { get; set; } = 1;
+        [Range(1, 100)] public int pageSize { get; set; } = 20;
+    }
+}
