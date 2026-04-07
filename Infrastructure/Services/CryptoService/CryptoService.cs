d36e1c8ddd0eb421c30f3ad4974279df041e1154
ManzanoYG <manzanoygonzalezmathias.pro@gmail.com>
2026-04-07 16:32:54 +0200

    Encrypt note content using AES

---
+++ Infrastructure/Services/CryptoService/CryptoService.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Services.CryptoService
+{
+    public class CryptoService
+    {
+        private readonly byte[] _key;
+
+        public CryptoService(IOptions<EncryptionSettings> options)
+        {
+            _key = Encoding.UTF8.GetBytes(options.Value.Key);
+        }
+
+        public string Encrypt(string plainText)
+        {
+            using var aes = Aes.Create();
+            aes.Key = _key;
+            aes.GenerateIV();
+
+            using var encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+            using var ms = new MemoryStream();
+
+            ms.Write(aes.IV, 0, aes.IV.Length);
+
+            using (var cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+            using (var sw = new StreamWriter(cs))
+            {
+                sw.Write(plainText);
+            }
+
+            return Convert.ToBase64String(ms.ToArray());
+        }
+
+        public string Decrypt(string cipherText)
+        {
+            var fullCipher = Convert.FromBase64String(cipherText);
+
+            using var aes = Aes.Create();
+            aes.Key = _key;
+
+            var iv = new byte[16];
+            Array.Copy(fullCipher, 0, iv, 0, iv.Length);
+            aes.IV = iv;
+
+            var cipher = new byte[fullCipher.Length - iv.Length];
+            Array.Copy(fullCipher, iv.Length, cipher, 0, cipher.Length);
+
+            using var decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+            using var ms = new MemoryStream(cipher);
+            using var cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read);
+            using var sr = new StreamReader(cs);
+
+            return sr.ReadToEnd();
+        }
+    }
+}
